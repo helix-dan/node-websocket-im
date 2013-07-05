7a0dcc2d18cf7b5117f077c85bf5c05100d76758
helix-dan <lotus.bat@gmail.com>
2013-07-05 17:48:00 +0800

    add style

--- im_server.js
+++ im_server.js
@@ -13,19 +13,31 @@ var wsServer = new WebSocketServer({
     httpServer: server
 });
 
+function htmlEntities(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
 wsServer.on('request', function(request){
     console.log('- -');
 
     var connection = request.accept(null, request.origin);
 
     connection.on('message', function(message){
-        if (message.type === 'utf8'){
-            console.dir(message.utf8Data);
-            connection.send(message.utf8Data)
-        }else{
-            connection.sendBytes(message.binaryData);
-            console.log('get byte data.');
-        }
+        console.log((new Date()) + ' Received Message from someone' + message.utf8Data);
+        // we want to keep history of all sent messages
+        var obj = {
+            time: (new Date()).getTime(),
+            text: htmlEntities(message.utf8Data),
+            author: 'some_one'
+        };
+
+        var json = JSON.stringify({ type:'message', data: obj });
+
+        connection.send(json);
+        // for (var i=0; i < clients.length; i++) {
+        //     clients[i].sendUTF(json);
+        // }
     });
 
     connection.on('close', function(connection){
